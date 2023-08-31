b2ae138e89a715f12919657a3ada342041a15f85
VIGNESH <vigneshmskt@gmail.com>
2023-09-01 02:08:26 +0530

    index page and name updated

--- src/components/navigation/auth-navigation.tsx
+++ src/components/navigation/auth-navigation.tsx
@@ -6,9 +6,9 @@ const AuthNavigation: FC = () => {
   return (
     <Box sx={{ '& button:first-child': { mr: 2 } }}>
       <StyledButton disableHoverEffect={true} variant="outlined">
-        Sign In
+        FiliBot Chat
       </StyledButton>
-      <StyledButton disableHoverEffect={true}>Sign Up</StyledButton>
+      <StyledButton disableHoverEffect={true}>FLN</StyledButton>
     </Box>
   )
 }
